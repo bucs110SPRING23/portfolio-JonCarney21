a0d1233c18ce2b6b7bb0940758436f9a3f324b31
Jon Carney <you@example.com>
2023-03-06 18:14:54 -0500

    hello

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -8,3 +8,41 @@ print("Cost per week:", cost_per_week)
 
 
 #Part B
+import random
+
+x = random.randrange(1,10)
+print(x)
+
+import turtle
+
+def main():
+    # Create two turtle objects
+    t1 = turtle.Turtle()
+    t2 = turtle.Turtle()
+    
+    # Place the turtles at their starting positions
+    t1.penup()
+    t1.goto(-100, 20)
+    t1.pendown()
+    
+    t2.penup()
+    t2.goto(-100, -20)
+    t2.pendown()
+    
+    # Move the turtles forward a random distance
+    for _ in range(10):
+        distance1 = random.randint(1, 100)
+        distance2 = random.randint(1, 100)
+        t1.forward(distance1)
+        t2.forward(distance2)
+    
+    # Reset the turtles to their starting position
+    t1.penup()
+    t1.goto(-100, 20)
+    t1.pendown()
+    
+    t2.penup()
+    t2.goto(-100, -20)
+    t2.pendown()
+
+
